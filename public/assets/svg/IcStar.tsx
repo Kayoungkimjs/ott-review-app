a2f141272d9e19ab4ec00d1f9c1c9b254ff783d8
Kim Kayoung <kykim.dev@gmail.com>
2024-05-10 15:00:23 +0900

    💄 Add thumbnails images to public folder

---
+++ public/assets/svg/IcStar.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from "react";
+const SvgIcStar = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 25"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M12.003 20.48 6.07 22.99c-.68.299-1.195-.081-1.127-.815l.543-6.422-4.21-4.874c-.502-.557-.298-1.154.435-1.33l6.273-1.454 3.313-5.512c.394-.652 1.018-.638 1.412 0l3.313 5.512 6.273 1.453c.733.177.923.774.434 1.33l-4.209 4.875.543 6.409c.068.747-.448 1.127-1.127.828z"
+    />
+  </svg>
+);
+export default SvgIcStar;
