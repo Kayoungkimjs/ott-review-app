2f4f0a9cfcda28c5c372ee8ff476388ccbba0207
Kim Kayoung <kykim.dev@gmail.com>
2024-05-11 12:33:19 +0900

    ✨ Set recoil root and global style in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { RecoilRoot } from 'recoil'
+import { Global, ThemeProvider } from '@emotion/react'
+import { theme } from './styles/index.ts'
+import { GlobalStyles } from './globalStyle.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyles} />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
 )
