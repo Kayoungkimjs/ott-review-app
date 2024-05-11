22254e0132788ad1b1f4fb4fde3f66d2a9a3042b
Kim Kayoung <kykim.dev@gmail.com>
2024-05-11 12:15:43 +0900

    🏷️ Add initial data and select option as constant

---
+++ src/constants/initialData.ts
@@ -0,0 +1,7 @@
+export const INITIALDATA: ReviewItems = {
+  id: 0,
+  category: 0,
+  title: '',
+  comment: '',
+  score: 0,
+}
---
+++ src/constants/selectOption.ts
@@ -0,0 +1,70 @@
+export const RATINGOPTION: RatingSelectOption[] = [
+  {
+    label: '1점',
+    value: 1,
+  },
+  {
+    label: '2점',
+    value: 2,
+  },
+  {
+    label: '3점',
+    value: 3,
+  },
+  {
+    label: '4점',
+    value: 4,
+  },
+  {
+    label: '5점',
+    value: 5,
+  },
+]
+
+export const CATEGORYOPTION: CategorySelectOption[] = [
+  {
+    label: 'Netflix (넷플릭스)',
+    value: 1,
+    image: 'netflix',
+  },
+  {
+    label: 'Watcha (왓챠)',
+    value: 2,
+    image: 'watcha',
+  },
+  {
+    label: 'Tving (티빙)',
+    value: 3,
+    image: 'tving',
+  },
+  {
+    label: 'Wavve (웨이브)',
+    value: 4,
+    image: 'wavve',
+  },
+  {
+    label: 'Disney+ (디즈니플러스)',
+    value: 5,
+    image: 'disney',
+  },
+  {
+    label: 'Coupang Play (쿠팡플레이)',
+    value: 6,
+    image: 'coupang',
+  },
+  {
+    label: 'Apple TV+ (애플티비)',
+    value: 7,
+    image: 'apple',
+  },
+  {
+    label: 'Amazon Prime (아마존프라임)',
+    value: 8,
+    image: 'amazon',
+  },
+  {
+    label: 'Laftel (라프텔)',
+    value: 9,
+    image: 'laftel',
+  },
+]
