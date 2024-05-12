1f32a96f8ec1b98b6577ee8db8eb431e96c17b8c
Kim Kayoung <kykim.dev@gmail.com>
2024-05-12 10:26:31 +0900

    🔧 Create jest modules for test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import svgr from 'vite-plugin-svgr'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,6 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.resolve('src') }],
   },
 
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), EnvironmentPlugin('all')],
   base: '/ott-review-app/',
 })
