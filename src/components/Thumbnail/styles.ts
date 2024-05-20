e246c60f63e7d244ac0c9474dc69e6a8555a5ffb
Kim Kayoung <kykim.dev@gmail.com>
2024-05-20 20:07:27 +0900

    ✨ Add border hover action on thumbnail

--- src/components/Thumbnail/styles.ts
+++ src/components/Thumbnail/styles.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import type { ThumbnailSize } from './index'
+import { theme } from '@/styles'
 
 interface StyledThumbnailProps {
   size: ThumbnailSize
@@ -14,11 +15,16 @@ export const StyledThumbnail = styled.div<StyledThumbnailProps>`
   background-repeat: no-repeat;
   background-size: contain;
   border-radius: 50%;
+  transition: border 200ms ease-in-out;
 
   img {
     display: block;
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+
+  &:hover {
+    border: 3px solid ${theme.colors.blue};
   }
 `
